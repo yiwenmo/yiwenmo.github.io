def7254a37f0f5845ee39e55d3ab3fe28534bb87
yiwenmo <winniemyiwen@gmail.com>
2024-02-02 15:33:52 +0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,109 @@
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
 
+//     // Navbar shrink function
+//     var navbarShrink = function () {
+//         const navbarCollapsible = document.body.querySelector('#mainNav');
+//         if (!navbarCollapsible) {
+//             return;
+//         }
+//         if (window.scrollY === 0) {
+//             navbarCollapsible.classList.remove('navbar-shrink')
+//         } else {
+//             navbarCollapsible.classList.add('navbar-shrink')
+//         }
+
+//     };
+
+//     // Shrink the navbar 
+//     navbarShrink();
+
+//     // Shrink the navbar when page is scrolled
+//     document.addEventListener('scroll', navbarShrink);
+
+//     //  Activate Bootstrap scrollspy on the main nav element
+//     const mainNav = document.body.querySelector('#mainNav');
+//     if (mainNav) {
+//         new bootstrap.ScrollSpy(document.body, {
+//             target: '#mainNav',
+//             rootMargin: '0px 0px -40%',
+//         });
+//     };
+
+//     // Collapse responsive navbar when toggler is visible
+//     const navbarToggler = document.body.querySelector('.navbar-toggler');
+//     const responsiveNavItems = [].slice.call(
+//         document.querySelectorAll('#navbarResponsive .nav-link')
+//     );
+//     responsiveNavItems.map(function (responsiveNavItem) {
+//         responsiveNavItem.addEventListener('click', () => {
+//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                 navbarToggler.click();
+//             }
+//         });
+//     });
+
+
+// });
+
+
+// 定義背景圖像陣列
+const backgroundImages = [
+    "../assets/img/background_img/NTU_fam.JPG",
+    "../assets/img/background_img/NTU_rihtengwu.jpg",
+    "../assets/img/background_img/NTU_run.jpg",
+    "../assets/img/background_img/SGIS_lab.jpg"
+  ];
+  
+  // 改變背景圖像的函式
+  function changeBackgroundOnScroll() {
+    const header = document.querySelector('.masthead');
+    const scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    const index = Math.floor(scrollPercentage * backgroundImages.length);
+    header.style.backgroundImage = `url(${backgroundImages[index]})`;
+  }
+  
+  // 監聽網頁滾動事件
+  window.addEventListener('scroll', changeBackgroundOnScroll);
+  
+  // 在DOMContentLoaded事件中的原有程式碼...
+  window.addEventListener('DOMContentLoaded', event => {
     // Navbar shrink function
     var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
+      const navbarCollapsible = document.body.querySelector('#mainNav');
+      if (!navbarCollapsible) {
+        return;
+      }
+      if (window.scrollY === 0) {
+        navbarCollapsible.classList.remove('navbar-shrink')
+      } else {
+        navbarCollapsible.classList.add('navbar-shrink')
+      }
     };
-
+  
     // Shrink the navbar 
     navbarShrink();
-
+  
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
-
+  
     //  Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
+      new bootstrap.ScrollSpy(document.body, {
+        target: '#mainNav',
+        rootMargin: '0px 0px -40%',
+      });
     };
-
+  
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
+    const responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
     responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+      responsiveNavItem.addEventListener('click', () => {
+        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+          navbarToggler.click();
+        }
+      });
     });
-
-
-});
+  });
+  
