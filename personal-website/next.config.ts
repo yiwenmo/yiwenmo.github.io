f9ae1f3881b3d7256b42808630f2c718fc76f320
Myiwen <winniemyiwen@gmail.com>
2025-07-13 13:20:26 +0800

    update next config

--- personal-website/next.config.ts
+++ personal-website/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  basePath: '/yiwenmo.github.io',
+  assetPrefix: '/yiwenmo.github.io',
 };
 
 export default nextConfig;
